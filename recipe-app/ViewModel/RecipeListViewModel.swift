4c02397b183daab1cfb51563b7f64aca2acca0e2
Arthur Hermann <arthurher22@gmail.com>
2024-12-03 10:13:49 +0100

    - moved private methods to an extension at the end of the file - moved RecipeError enum to seperate file

--- recipe-app/ViewModel/RecipeListViewModel.swift
+++ recipe-app/ViewModel/RecipeListViewModel.swift
@@ -10,34 +10,7 @@ import UIKit
 final class RecipeListViewModel {
     private let apiKey = "5b91aa819e6d4bc8848f4c972103e6dc"
     var recipes = [Recipe]()
-    
-    private func makeURL(query: String) -> URL? {
-        var components = URLComponents(string: "https://api.spoonacular.com/recipes/complexSearch")
-        components?.queryItems = [
-            URLQueryItem(name: "apiKey", value: apiKey),
-            URLQueryItem(name: "query", value: query),
-            URLQueryItem(name: "number", value: "20")
-        ]
-        return components?.url
-    }
-    //parses JSON files
-    private func parse(json: Data) throws -> [Recipe] {
-        do {
-            let decoder = JSONDecoder()
-            let jsonRecipes = try decoder.decode(Recipes.self, from: json)
-            recipes = jsonRecipes.results
-            
-        }
-        catch  {
-            RecipeError.parsingError
-        }
-        return recipes
-    }
-    
-    private enum RecipeError: Error {
-        case invalidURL, networkError, unexpectedResponse, noData, parsingError
-    }
-    
+  
     func searchRecipe(_ query: String,
                       completion: @escaping (Result<[Recipe], Error>) -> Void)  {
         guard let url = makeURL(query: query) else {
@@ -60,7 +33,7 @@ final class RecipeListViewModel {
                 return
             }
             do {
-                let recipes = try self.parse(json: data)
+                let recipes = try parse(json: data)
                 DispatchQueue.main.async {
                     self.recipes = recipes
                     completion(.success(recipes))
@@ -71,3 +44,30 @@ final class RecipeListViewModel {
         }.resume()
     }
 }
+        
+        //MARK: - Helpers
+        extension RecipeListViewModel {
+            private func makeURL(query: String) -> URL? {
+                var components = URLComponents(string: "https://api.spoonacular.com/recipes/complexSearch")
+                components?.queryItems = [
+                    URLQueryItem(name: "apiKey", value: apiKey),
+                    URLQueryItem(name: "query", value: query),
+                    URLQueryItem(name: "number", value: "20")
+                ]
+                return components?.url
+            }
+            //parses JSON files
+            private func parse(json: Data) throws -> [Recipe] {
+                do {
+                    let decoder = JSONDecoder()
+                    let jsonRecipes = try decoder.decode(Recipes.self, from: json)
+                    recipes = jsonRecipes.results
+                    
+                }
+                catch  {
+                    RecipeError.parsingError
+                }
+                return recipes
+            }
+        }
+
